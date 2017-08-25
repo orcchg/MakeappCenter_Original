8d53b74f7a9963fd934b33cb573b8e25a0bb08c0
Maxim Alov <maxim@aludio.com>
2017-08-25 17:07:23 +0300

    Bones 4

--- data/src/main/kotlin/com/orcchg/makeappcenter/data/repository/product/ProductRepository.kt
+++ data/src/main/kotlin/com/orcchg/makeappcenter/data/repository/product/ProductRepository.kt
@@ -1,9 +1,15 @@
 package com.orcchg.makeappcenter.data.repository.product
 
 import com.orcchg.makeappcenter.data.source.local.product.ProductDao
+import com.orcchg.makeappcenter.domain.model.Product
+import io.reactivex.Flowable
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ProductRepository @Inject constructor(productDao: ProductDao) {
+
+    fun products(): Flowable<List<Product>> {
+        //
+    }
 }
--- data/src/main/kotlin/com/orcchg/makeappcenter/data/viewmodel/product/ProductViewModel.kt
+++ data/src/main/kotlin/com/orcchg/makeappcenter/data/viewmodel/product/ProductViewModel.kt
@@ -4,4 +4,6 @@ import android.arch.lifecycle.ViewModel
 import com.orcchg.makeappcenter.data.repository.product.ProductRepository
 
 class ProductViewModel(private val repository: ProductRepository) : ViewModel() {
+
+    fun products() = repository.products()
 }
